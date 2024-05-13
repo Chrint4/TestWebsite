350fa1641ebf8365a57c2c344f223d5782c59199
Chrint4 <christophermorley10@gmail.com>
2024-05-13 01:18:49 +0100

    redo

--- script.js
+++
@@ -1,9 +0,0 @@
-fetch("foo.txt").then(function (res) {
-    return res.text();
-}).then(function (data) {
-    let i = 1;
-    data.split("||").forEach(function (element) {
-        document.getElementById("text" + i).innerHTML = element.trim();
-        i++;
-    });
-});
